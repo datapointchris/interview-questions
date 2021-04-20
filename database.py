9742ad84b09f7c94e53a5742594248dcfb2893ed
Chris Birch <chrisbirch@live.com>
2021-04-19 20:26:23 -0400

    feat: Change to SQLite database
    Fixes #11

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 import sqlite3
 
 
+
 class DatabaseManager:
-    def __init__(self, database_file):
-        self.connection = sqlite3.connect(database_file)
+    def __init__(self, database_filename):
+        self.connection = sqlite3.connect(database_filename)
 
     def __del__(self):
         self.connection.close()
@@ -15,17 +16,74 @@ class DatabaseManager:
             return cursor
 
     def create_table(self, table_name, columns):
-        columns_and_types = [f'{column_name} {data_type}'
-                             for column_name, data_type in columns.items()]
-
+        columns_with_types = [
+            f'{column_name} {data_type}'
+            for column_name, data_type in columns.items()
+        ]
         self._execute(
             f'''
             CREATE TABLE IF NOT EXISTS {table_name}
-            ({', '.join(columns_and_types)});
+            ({', '.join(columns_with_types)});
             '''
         )
     
+    def add(self, table_name, data):
+        placeholders = ', '.joint('?' * len(data))
+        column_names = ', '.join(data.keys())
+        column_values = tuple(data.values())
+
+        self._execute(
+            f'''
+            INSERT INTO {table_name}
+            ({column_names})
+            VALUES ({placeholders});
+            ''',
+            column_values,
+        )
+
+    def delete(self, table_name, criteria):
+        placeholders = [f'{column} = ?' for column in criteria.keys()]
+        delete_criteria = ' AND '.join(placeholders)
+        self._execture(
+            f'''
+            DELETE FROM {table_name}
+            WHERE {delete_criteria};
+            ''',
+            tuple(criteria.values()),
+        )
+
+    def select(self, table_name, criteria=None, order_by=None):
+        criteria = criteria or {}
+
+        query = f'SELECT * FROM {table_name}'
+
+        if criteria:
+            placeholders = [f'{column} = ?' for column in criteria.keys()]
+            select_criteria = ' AND '.join(placeholders)
+            query += f' WHERE {select_criteria}'
+
+        if order_by:
+            query += f' ORDER BY {order_by}'
+
+        return self._execute(
+            query,
+            tuple(criteria.values()),
+        )
+
     def drop_table(self, table_name):
         self._execute(f'DROP TABLE {table_name}')
 
+    def update(self, table_name, data):
+        placeholders = ', '.joint('?' * len(data))
+        column_names = ', '.join(data.keys())
+        column_values = tuple(data.values())
+
+        self._execute(
+            f'''
+            INSERT INTO {table_name}
+            ({column_names})
+            VALUES ({placeholders});
+            ''',
+            column_values,
+        )
     
